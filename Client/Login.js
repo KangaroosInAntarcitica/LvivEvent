83e04f4b4ef2fa14f13d16b3a7a303210f6f3c00
tolik0 <iatsuk@ucu.edu.ua>
2018-01-23 17:41:43 +0200

    Правки в дизайн

--- Client/Login.js
+++ Client/Login.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
-import { AppRegistry, Text, View, StyleSheet, Image, TextInput, TouchableOpacity, Dimensions } from 'react-native';
+import {AppRegistry, Text, View, StyleSheet, Image, TextInput, TouchableOpacity, Dimensions} from 'react-native';
 
 
 // properties: url, onSuccess, register
-export default class Login extends Component{
-    constructor(properties){
+export default class Login extends Component {
+    constructor(properties) {
         super();
         this.state = {
-            message: "Please, log in!"
+            message: ""
         };
     }
-    submit(){
-        if(this.state.username && this.state.password){
+
+    submit() {
+        if (this.state.username && this.state.password) {
             this.setState({message: "Loading"});
 
             let url = this.props.url;
@@ -31,46 +32,66 @@ export default class Login extends Component{
                 this.setState({message})
 
                 // do the default on success activity
-                if(data.status == 'success')
+                if (data.status == 'success')
                     this.props.onSuccess();
             }).catch(() => {
                 this.setState({message: "Error occurred!"})
             });
         }
     }
-    render(){
-        return(
+
+    render() {
+        return (
             <View style={styles.page}>
-                <Image source={require('./images/LogoNew.png')} style={styles.logo}/>
-                <Text style={styles.information} > { this.state.message } </Text>
+                <Image source={require('./images/sp2.png')} style={styles.logo}/>
+                <Text style={styles.information}> {this.state.message} </Text>
                 <View style={styles.form}>
-                    <TextInput placeholder="username"
-                        onChangeText={(username) => { this.setState({username: username}) } }
-                        onSubmitEditing={() => { this.refs.password.focus(); } }
+                    <TextInput
+                        placeholder="Login"
+                        onChangeText={(username) => {
+                            this.setState({username: username})
+                        }}
+                        onSubmitEditing={() => {
+                            this.refs.password.focus();
+                        }}
                         style={styles.input}
-                        underlineColorAndroid="transparent"
+                        returnKeyType="go"
                     />
                     <TextInput
                         ref="password"
-                        placeholder="password"
-                        onChangeText={(password) => { this.setState({password: password}) } }
-                        onSubmitEditing={ this.submit.bind(this) }
+                        placeholder="Password"
+                        secureTextEntry
+                        onChangeText={(password) => {
+                            this.setState({password: password})
+                        }}
+                        onSubmitEditing={this.submit.bind(this)}
                         style={styles.input}
-                        underlineColorAndroid="transparent"
                     />
-                    <TouchableOpacity onPress={ this.submit.bind(this) } style={styles.button}>
-                        <Text style={styles.buttonText}> Submit </Text>
+                    <TouchableOpacity onPress={this.submit.bind(this)} style={styles.button}>
+                        <Text style={styles.buttonText}> Log in </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={ this.props.register } style={styles.buttonRegister} >
-                        <Text style={styles.buttonText}> Register </Text>
+                </View>
+                <View style={styles.registercontainer}>
+                    <TouchableOpacity onPress={this.props.register}>
+                        <Text style={styles.text}>Create Account</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.props.register}>
+                        <Text style={styles.text}>Forgot Password?</Text>
                     </TouchableOpacity>
                 </View>
             </View>
-        );
+        )
+            ;
     }
 }
 
 const styles = StyleSheet.create({
+    registercontainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20
+    },
     page: {
         width: "100%",
         height: "100%",
@@ -94,17 +115,14 @@ const styles = StyleSheet.create({
     input: {
         paddingHorizontal: 15,
         paddingVertical: 5,
+        paddingBottom: 15,
         margin: 5,
         height: 40,
         fontSize: 16,
-        borderWidth: 2,
-        borderColor: "#BBBBBB",
-        borderRadius: 50,
-        backgroundColor: "#EFEFEF",
         width: "100%"
     },
     button: {
-        paddingVertical: 5,
+        marginTop: 30,
         margin: 5,
         height: 40,
         borderRadius: 20,
@@ -112,17 +130,15 @@ const styles = StyleSheet.create({
         width: "100%"
     },
     buttonText: {
+        paddingVertical: 7,
         textAlign: "center",
-        fontSize: 22,
+        fontSize: 17,
         color: "#FFFFFF"
     },
-    buttonRegister: {
-        paddingVertical: 5,
-        margin: 5,
-        height: 40,
-        borderRadius: 20,
-        backgroundColor: "#a3ddf1",
-        width: "100%"
+    text: {
+        color: '#999999',
+        backgroundColor: 'transparent',
+        paddingHorizontal: 12,
     },
     form: {
         width: "100%",
--- Client/Register.js
+++ Client/Register.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { AppRegistry, Text, View, StyleSheet, Image, TextInput, TouchableOpacity, Dimensions } from 'react-native';
+import { Header, AppRegistry, Text, View, StyleSheet, Image, TextInput, TouchableOpacity, Dimensions } from 'react-native';
 
 
 // properties: url, onSuccess, register
@@ -7,12 +7,12 @@ export default class Login extends Component{
     constructor(properties){
         super();
         this.state = {
-            message: "Create new account"
+            message: ""
         };
     }
     submit(){
         if (this.state.password !== this.state.passwordRepeat){
-            this.setState({message: "Passwords did not match!"})
+            this.setState({message: "Wrong password repeat!"})
         }
         else if(this.state.username && this.state.email && this.state.password){
             this.setState({message: "Loading"});
@@ -44,46 +44,50 @@ export default class Login extends Component{
     }
     render(){
         return(
-            <View style={styles.page}>
-                <Text style={styles.information} > { this.state.message } </Text>
-                <View style={styles.form}>
-                    <TextInput
-                        placeholder="username"
-                        onChangeText={(username) => { this.setState({username: username}) } }
-                        onSubmitEditing={() => { this.refs.email.focus() } }
-                        style={styles.input}
-                        underlineColorAndroid="transparent"
-                    />
-                    <TextInput
-                        ref="email"
-                        placeholder="e-mail"
-                        onChangeText={(email) => { this.setState({email: email}) } }
-                        onSubmitEditing={ () => { this.refs.password.focus() } }
-                        style={styles.input}
-                        underlineColorAndroid="transparent"
-                    />
-                    <TextInput
-                        ref="password"
-                        placeholder="password"
-                        onChangeText={(password) => { this.setState({password: password}) } }
-                        onSubmitEditing={ () => { this.refs.passwordRepeat.focus() } }
-                        style={styles.input}
-                        underlineColorAndroid="transparent"
-                    />
-                    <TextInput
-                        ref="passwordRepeat"
-                        placeholder="repeat password"
-                        onChangeText={(passwordRepeat) => { this.setState({passwordRepeat: passwordRepeat}) } }
-                        onSubmitEditing={ this.submit.bind(this) }
-                        style={styles.input}
-                        underlineColorAndroid="transparent"
-                    />
-                    <TouchableOpacity onPress={ this.submit.bind(this) } style={styles.button}>
-                        <Text style={styles.buttonText}> Register </Text>
+            <View>
+                <View style={styles.header}>
+                    <TouchableOpacity style={{width: 50, height: 50}} onPress={this.props.login} >
+                        <Image style={{width: 30, height: 30, position: 'absolute', top: 10, left: 10}} source={require('./images/backArrow.png')} />
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={ this.props.login } style={styles.buttonRegister} >
-                        <Text style={styles.buttonText}> Back to Login </Text>
-                    </TouchableOpacity>
+                    <Text style={{color: '#FFFFFF', fontSize: 24, padding: 10}}> Registration </Text>
+                    <View style={{width: 50}}/>
+                </View>
+                <View style={styles.page}>
+                    <Text style={styles.information} > { this.state.message } </Text>
+                    <View style={styles.form}>
+                        <TextInput
+                            placeholder="username"
+                            onChangeText={(username) => { this.setState({username: username}) } }
+                            onSubmitEditing={() => { this.refs.email.focus() } }
+                            style={styles.input}
+                        />
+                        <TextInput
+                            ref="email"
+                            placeholder="e-mail"
+                            onChangeText={(email) => { this.setState({email: email}) } }
+                            onSubmitEditing={ () => { this.refs.password.focus() } }
+                            style={styles.input}
+                        />
+                        <TextInput
+                            ref="password"
+                            secureTextEntry
+                            placeholder="password"
+                            onChangeText={(password) => { this.setState({password: password}) } }
+                            onSubmitEditing={ () => { this.refs.passwordRepeat.focus() } }
+                            style={styles.input}
+                        />
+                        <TextInput
+                            ref="passwordRepeat"
+                            secureTextEntry
+                            placeholder="repeat password"
+                            onChangeText={(passwordRepeat) => { this.setState({passwordRepeat: passwordRepeat}) } }
+                            onSubmitEditing={ this.submit.bind(this) }
+                            style={styles.input}
+                        />
+                        <TouchableOpacity onPress={ this.submit.bind(this) } style={styles.button}>
+                            <Text style={styles.buttonText}> Register </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         );
@@ -91,12 +95,18 @@ export default class Login extends Component{
 }
 
 const styles = StyleSheet.create({
+    registercontainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20
+    },
     page: {
-        width: "100%",
-        height: "100%",
+        width: '100%',
+        height: Dimensions.get('window').height - 50,
         padding: "10%",
         backgroundColor: "#FFFFFF",
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center"
     },
     logo: {
@@ -114,17 +124,14 @@ const styles = StyleSheet.create({
     input: {
         paddingHorizontal: 15,
         paddingVertical: 5,
+        paddingBottom: 15,
         margin: 5,
         height: 40,
         fontSize: 16,
-        borderWidth: 2,
-        borderColor: "#BBBBBB",
-        borderRadius: 50,
-        backgroundColor: "#EFEFEF",
         width: "100%"
     },
     button: {
-        paddingVertical: 5,
+        marginTop: 30,
         margin: 5,
         height: 40,
         borderRadius: 20,
@@ -132,20 +139,27 @@ const styles = StyleSheet.create({
         width: "100%"
     },
     buttonText: {
+        paddingVertical: 7,
         textAlign: "center",
-        fontSize: 22,
+        fontSize: 17,
         color: "#FFFFFF"
     },
-    buttonRegister: {
-        paddingVertical: 5,
-        margin: 5,
-        height: 40,
-        borderRadius: 20,
-        backgroundColor: "#a3ddf1",
-        width: "100%"
+    text: {
+        color: '#999999',
+        backgroundColor: 'transparent',
+        paddingHorizontal: 12,
     },
     form: {
         width: "100%",
-        maxWidth: 260
+        maxWidth: 260,
+        height: 176
+    },
+
+    header: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#36A9E1',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     }
 });
